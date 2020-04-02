e1238b869721dc598d839753580f0ec651bb394f
Alejandro Fdez <fertry.contacto@gmail.com>
2020-04-02 14:09:04 +0200

    Cambios en el fomrulario, headers e imagenes
    * Cambiados los nombres de todas las imágnes del proyecto
    * Eliminado el archivo error_reserva.php
    * Eliminado el archivo formulario_reserva.php
    * Actualizado la página reservas.php
    
    * La parte JS de reservas.php está pendiente junto con las funciones abrir/cerrar conexion BD

---
+++ header_reservas.php
@@ -0,0 +1,9 @@
+<header class = "header_reservas">
+
+    <div class = "header">
+
+        <img src = "images\header_reservas.png" width = "100%" />
+
+    </div>
+
+</header>
---
+++ header_ubicacion.php
@@ -0,0 +1,9 @@
+<header class = "header_ubicacion">
+
+    <div class = "header">
+
+        <img src = "images\header_ubicacion.png" width = "100%" />
+
+    </div>
+
+</header>
