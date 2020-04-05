49a1572c69dd1d679b6e2d60eb8f241ce56cbc9f
Alejandro Fdez <fertry.contacto@gmail.com>
2020-04-05 18:24:51 +0200

    Cambios acceso a BD

--- carta.php
+++ carta.php
@@ -2,6 +2,67 @@
 
     session_start();
 
+    // Llamamos a gestionBD.php y a paginacion_consulta.php
+    require_once("gestionBD.php");
+    require_once("paginacion_consulta.php");
+
+    if (isset($_SESSION["paginacion"])) {
+
+        $paginacion = $_SESSION["paginacion"];
+
+    }
+
+    // La página por defecto será la primera a menos que se cambie y el tamaño por defecto será de 5:
+    $pagina_seleccionada = isset($_GET["PAG_NUM"]) ? (int)$_GET["PAG_NUM"] : (isset($paginacion) ? (int)$paginacion["PAG_NUM"] : 1);
+    $pag_tam = isset($_GET["PAG_TAM"]) ? (int)$_GET["PAG_TAM"] : (isset($paginacion) ? (int)$paginacion["PAG_TAM"] : 5);
+
+    if ($pagina_seleccionada < 1) {
+
+        $pagina_seleccionada = 1;
+
+    }
+
+    if ($pag_tam < 1) {
+
+        $pag_tam = 5;
+
+    }	
+
+    unset($_SESSION["paginacion"]);
+
+    // Abrimos la conexion con la BD:
+    $conexion = abrirConexionBD();
+
+    // Consulta: nombre y precio de todos los platos disponibles:
+    $query = "SELECT nombre, precioProducto FROM Productos WHERE disponibilidad = 1";
+
+    // Se comprueba que el tamaño de página, página seleccionada y total de registros son conformes.
+    $total_registros = total_consulta($conexion, $query);
+    $total_paginas = (int)($total_registros / $pag_tam);
+
+    if ($total_registros % $pag_tam > 0) {
+
+        $total_paginas++;
+
+    }		
+
+    if ($pagina_seleccionada > $total_paginas) {
+
+        $pagina_seleccionada = $total_paginas;
+
+    }		
+
+    // Guardamos los valores generados:
+    $paginacion["PAG_NUM"] = $pagina_seleccionada;
+    $paginacion["PAG_TAM"] = $pag_tam;
+    $_SESSION["paginacion"] = $paginacion;
+
+    // Datos que se van a mostrar: 
+    $filas = consulta_paginada($conexion, $query, $pagina_seleccionada, $pag_tam);
+
+    // Cerramos la conexión:
+    cerrarConexionBD($conexion);
+
 ?>
 
 <!DOCTYPE html>
@@ -38,20 +99,80 @@
     ?>
     <br>
 
-    <!-- Paginación -->
-    <!-- Carta del restaurante -->
-    <div class = "paginacion">
+    <main>
 
-        <a href="#">&laquo;</a>
-        <a href="#">1</a>
-        <a class="activo" href="#">2</a>
-        <a href="#">3</a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-        <a href="#">6</a>
-        <a href="#">&raquo;</a>
+        <nav>
 
-    </div> 
+            <div id="enlaces">
+
+                <?php
+
+                for($pagina = 1; $pagina <= $total_paginas; $pagina++)
+
+                    if ($pagina == $pagina_seleccionada) { 	
+
+                ?>
+
+                        <span class="current"><?php echo $pagina; ?></span>
+
+                <?php
+
+                    } else {
+
+                ?>
+
+                        <a href="consulta_libros.php?PAG_NUM=<?php echo $pagina; ?>&PAG_TAM=<?php echo $pag_tam; ?>"><?php echo $pagina; ?></a>
+
+                <?php
+                
+                    }
+
+                ?>
+                        
+            </div>
+
+            <form method = "get" action = "carta.php">
+
+                <div>
+
+                    <input id="PAG_NUM" name = "PAG_NUM" type = "hidden" value = "<?php echo $pagina_seleccionada?>"/>
+
+                </div>
+
+                <div>
+
+                    Mostrando 
+                    <input id = "PAG_TAM" name = "PAG_TAM" type="number" min = "1" max = "<?php echo $total_registros; ?>" value = "<?php echo $pag_tam?>" autofocus/>
+                    entradas de <?php echo $total_registros?>
+
+                    <input type = "submit" value = "Cambiar">
+
+                </div>
+
+            </form>
+
+        </nav>
+                
+    <?php
+
+        foreach($filas as $fila) {
+
+    ?>
+
+            <!-- Mostrar los datos solicitados -->
+            <article>
+
+                <h4><?php echo $fila ?></h4>
+
+            </article>
+
+    <?php
+
+        }
+
+    ?>
+
+    </main>
 
     <?php 
 
